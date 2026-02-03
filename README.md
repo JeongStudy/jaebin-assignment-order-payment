7b055b869db9fb2be5902a9f21f78d41c6ff9dab
jaebin1234 <jaebin2586@gmail.com>
2026-02-03 17:04:53 +0900

    feat: 과제 테스트 프로젝트 초기 셋팅

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jaebin-assignment-order-payment
+
 재빈 백엔드 과제 주문/결제 시스템
---
+++ src/main/java/com/assignment/jaebinassignmentorderpayment/JaebinAssignmentOrderPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.jaebinassignmentorderpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaebinAssignmentOrderPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaebinAssignmentOrderPaymentApplication.class, args);
+	}
+
+}
